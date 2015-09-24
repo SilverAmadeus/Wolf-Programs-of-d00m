bccac27daae234b837e6c7510625210dff5bf1cf
SilverAmadeus <Ivan@Ivans-MacBook-Pro.local>
2015-09-24 17:56:54 -0500

    Proceso 1 completo
    Proceso 1 ya actual como, implementé los print que voy a usar en la
    clase “checker” que llevará los porcentajes de memoria de los procesos
    y CPU de forma constante en el monitor.

--- Proyecto/CPU.py
+++ Proyecto/CPU.py
@@ -13,13 +13,14 @@ class CPU(object):
         print("Iniciando repartición de memoria")
         p1.memory = self.memory[:int((len(self.memory)) * .4)] #Tomando los bloques del cpu
         self.memory = [0] * (self.max_size - len(p1.memory))
+
         # newList = originalList[int(len(originalList) * .05) : int(len(originalList) * .95)]
         return 0
 
 cpu = CPU()
-print(len(cpu.memory))
+print("Memoria de CPU inicial " + str(len(cpu.memory)))
 cpu.initialize()
-print(len(p1.memory))
-print(len(cpu.memory))
+print("Memoria de CPU: " + str(len(cpu.memory)))
+print("Memoria de Proceso 1: " +str(len(p1.memory)))
 p1t = threading.Thread(target=p1.tasks, args=()) # Thread de Process 1
 p1t.start()
--- Proyecto/Proceso_1.py
+++ Proyecto/Proceso_1.py
@@ -2,26 +2,38 @@ __author__ = 'Ivan'
 import random, time
 class Process1(object):
     def __init__(self):
+        self.max_size = 0
         self.memory = []
         self.status = True
+        self.probability = 0
 
     def initialize(self):
         print("Este es el proceso 1")
 
         return 0
     def tasks(self):
+        self.max_size = len(self.memory)
         while self.status is True:
             i = random.randint(0, 99)
-            print("Proceso 1: Trabajando...")
+            print("Proceso 1 (Tamaño: " + str(len(self.memory))+ ") " +str(i)+": Trabajando...",  end='\r')
             time.sleep(2)
-            print(i)
-            if (i < 40):
-                print("Proceso 1: Eliminando...")
+            if len(self.memory) < 3:
+                self.probability = 80
+            elif len(self.memory) == 8:
+                self.probability = 30
+
+            if (i > self.probability):
+                print("Proceso 1: Eliminando...", end='\r')
                 time.sleep(2)
                 self.memory.pop()
-                print("Proceso 1: Tamaño de bloque ", len(self.memory))
+                print("Proceso 1: Tamaño de bloque ", len(self.memory), end='\r')
                 if not self.memory:
-                    print("Proceso 1 sin memoria")
+                    print("Proceso 1 sin memoria", end='\r')
                     self.status = False
+            else:
+                if len(self.memory) == 8:
+                    print("Proceso 1: Memoria maxima",  end='\r')
+                else:
+                    self.memory.append(0)
 
         return 0
