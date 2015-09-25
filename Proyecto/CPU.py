96f5086a4c674de7f1c08171fb720914a4030c82
SilverAmadeus <Ivan@Ivans-MacBook-Pro.local>
2015-09-25 16:30:37 -0500

    CAMARAKJHDJAGJDHSAGDFJASFDFS
    FAYGSDFYAHDSGFAS

--- Proyecto/CPU.py
+++ Proyecto/CPU.py
@@ -1,15 +1,32 @@
 __author__ = 'Big Boss'
-import threading, random, time
+import threading, random, time, os, sys
 from tkinter import *
 gui = Tk()
 
+
+
 # Cambiar el numero del proceso 3 a 4 ya que usan la misma clase pero son diferentes instancias
+
 
 class CPU(object):
     def __init__(self):
         self.max_size = 50
         self.memory = [0] * self.max_size
+        self.console_cpu = False
+        self.console_mem1 = False
+        self.status = True
 
+    def console_stat_cpu(self):
+        if self.console_cpu is True:
+            self.console_cpu = False
+        else:
+            self.console_cpu = True
+
+    def console_stat_mem1(self):
+        if self.console_mem1 is True:
+            self.console_mem1 = False
+        else:
+            self.console_mem1 = True
 
     def initialize(self):
         global CPU_memory
@@ -23,11 +40,10 @@ class CPU(object):
         p3.memory = self.memory[:int((len(self.memory)) * .25)] #Tomando los bloques del cpu para proceso 3
         self.memory = [0] * (len(self.memory)- len(p3.memory))
 
-        p4.memory = self.memory[:int((len(self.memory)) * .25)] #Tomando los bloques del cpu para proceso 3
+        p4.memory = p3.memory #Tomando los bloques del cpu para proceso 3
         self.memory = [0] * (len(self.memory)- len(p4.memory))
         CPU_memory = self.memory
 
-        p4.memory = [0] *5
         # newList = originalList[int(len(originalList) * .05) : int(len(originalList) * .95)]
         return 0
 
@@ -43,7 +59,15 @@ class CPU(object):
             time.sleep(2)
             return False
 
-
+    def percentages(self):
+        global CPU_memory
+        while self.status is True:
+            increase = float(len(CPU_memory)) - float(self.max_size)
+            decrease = float(self.max_size) - float(len(CPU_memory))
+            free_cpu_percent = ((increase / float(self.max_size)) * 100)+100
+            used_cpu_percent = ((decrease/float(self.max_size))*100)
+            if self.console_cpu is True: print("Memoria libre de CPU: "+str(free_cpu_percent)+ "%" + "\tMemoria en uso de CPU: "+ str(used_cpu_percent)+"%", end ='\r')
+            time.sleep(2)
 
 class Process1(CPU):
     def __init__(self):
@@ -64,18 +88,15 @@ class Process1(CPU):
     def console_stat(self):
         if self.console is True:
             self.console = False
-            print(self.console)
         else:
             self.console = True
-            print(self.console)
 
-        return 0
     def tasks(self):
         global CPU_memory
         self.max_size = len(self.memory)
         while self.status is True:
             i = random.randint(0, 99)
-            if self.console is True: print("Proceso 1 (Tamaño: " + str(len(self.memory))+ ") " +str(i)+": Trabajando...", end='\r')
+            if self.console is True: print("Proceso 1 (Tamaño: " + str(len(self.memory))+ ") "+": Trabajando...", end='\r')
             time.sleep(2)
             if len(self.memory) < 5:
                 self.probability = 80
@@ -83,7 +104,7 @@ class Process1(CPU):
                 self.probability = 30
 
             if (i > self.probability):
-                if self.console is True: print("Proceso 1: Eliminando...", end ='\r')
+                if self.console is True: print("Proceso 1 (Tamaño: " + str(len(self.memory))+ ") "+": Eliminando...", end ='\r')
                 #print("Proceso 1: Tamaño de bloque ", len(self.memory), end='\r')
                 time.sleep(2)
                 self.memory.pop()
@@ -127,7 +148,6 @@ class Process2(CPU):
         else:
             self.console = True
 
-        return 0
     def tasks(self):
         global CPU_memory
         self.max_size = len(self.memory)
@@ -141,7 +161,7 @@ class Process2(CPU):
                 self.probability = 25
 
             if (i > self.probability):
-                if self.console is True: print("Proceso 2: Eliminando...", end='\r')
+                if self.console is True: print("Proceso 1 (Tamaño: " + str(len(self.memory))+ ") "+": Eliminando...", end ='\r')
                 time.sleep(2)
                 self.memory.pop()
                 #print("Proceso 2: Tamaño de bloque ", len(self.memory))
@@ -198,7 +218,7 @@ class Process3(CPU):
                 self.probability = 15
 
             if (i > self.probability):
-                if self.console is True: print("Proceso 3: Eliminando...", end='\r')
+                if self.console is True: print("Proceso 1 (Tamaño: " + str(len(self.memory))+ ") "+": Eliminando...", end ='\r')
                 time.sleep(2)
                 self.memory.pop()
                 if self.console is True: print("Proceso 3: Tamaño de bloque ", len(self.memory), end='\r')
@@ -229,6 +249,7 @@ class Process3(CPU):
  #       self.memory = 0
 
   #  def initialize(self):
+global CPU_memory
 p1 = Process1()
 p2 = Process2()
 p3 = Process3()
@@ -247,15 +268,18 @@ p1t = threading.Thread(target=p1.tasks, args=())
 p2t = threading.Thread(target=p2.tasks, args=()) # Thread de Process 1
 p3t = threading.Thread(target=p3.tasks, args=())
 #p4t = threading.Thread(target=p4.tasks(), args=())
+cput = threading.Thread(target=cpu.percentages, args=())
 p1t.start()
 p2t.start()
 p3t.start()
 #p4t.start()
+cput.start()
 buttonPROCESS1 = Button (gui, text="Process 1",command = p1.console_stat).place(x=50,y=50)
 # gui.quit se sustituye por la funcion que deseemos
 buttonPROCESS2 = Button (gui, text="Process 2",command = p2.console_stat).place(x=50,y=80)
 buttonPROCESS3 = Button (gui, text="Process 3",command = p3.console_stat).place(x=50,y=110)
+buttonPROCESSCPU = Button (gui, text="Procesos en CPU",command = cpu.console_stat_cpu).place(x=400,y=70)
 
-gui.geometry("200x200")#dimensiones ancho x alto
+gui.geometry("600x200")#dimensiones ancho x alto
 gui.title("Memory Management Unit")
 gui.mainloop()
